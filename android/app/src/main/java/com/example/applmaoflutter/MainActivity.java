781a119f9d761ce6760b1d234a8bb9678f77e161
seugirdur <gabriel.lima137170@gmail.com>
2022-09-18 11:28:11 -0300

    Initial commit

---
+++ android/app/src/main/java/com/example/applmaoflutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.applmaoflutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(Container(
+    child: Center(
+      child: Text(
+        "get on shine on",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.white, fontSize: 40.0)),
+    ),
+  ));
+}
